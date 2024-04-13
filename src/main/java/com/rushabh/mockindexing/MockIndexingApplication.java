2f9119b5ac816c2284a5ff66e0115b1a18f2bc52
RUSHABH82 <makanirushabh0786@gmail.com>
2024-04-13 22:29:55 +0530

    initial push

---
+++ src/main/java/com/rushabh/mockindexing/MockIndexingApplication.java
@@ -0,0 +1,13 @@
+package com.rushabh.mockindexing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockIndexingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MockIndexingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rushabh/mockindexing/MockIndexingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rushabh.mockindexing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockIndexingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
